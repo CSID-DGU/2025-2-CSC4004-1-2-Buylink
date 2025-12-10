aa8c6d492812d66a22ac77aa0d3db0c46e602583
zienyyyy <zienyyyy@gmail.com>
2025-12-11 02:37:05 +0900

    Wrap App component with RecoilRoot

--- FE/buylink/src/main.tsx
+++ FE/buylink/src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { RecoilRoot } from "recoil"; // ✅ 추가
+import { RecoilRoot } from "recoil";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RecoilRoot>   {/* ✅ 반드시 App을 감싸야 함 */}
+    <RecoilRoot>
       <App />
     </RecoilRoot>
   </React.StrictMode>
