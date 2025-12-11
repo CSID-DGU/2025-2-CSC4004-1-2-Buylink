5be49b64fd8b07806e5ff8c38460a0e4af1fda3d
hayo02 <hayo021111@naver.com>
2025-12-12 00:43:26 +0900

    Refactor: Update OrderCreateRequest fields and validation rules

--- BE/proxy-shopping/src/main/java/io/github/hayo02/proxyshopping/orders/dto/OrderCreateRequest.java
+++ BE/proxy-shopping/src/main/java/io/github/hayo02/proxyshopping/orders/dto/OrderCreateRequest.java
@@ -1,4 +1,3 @@
-// src/main/java/io/github/hayo02/proxyshopping/orders/dto/OrderCreateRequest.java
 package io.github.hayo02.proxyshopping.orders.dto;
 
 import jakarta.validation.constraints.NotNull;
@@ -12,15 +11,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class OrderCreateRequest {
 
-    // 배송지 id (ShippingAddress id)
+
     @NotNull
     private Long addressId;
 
-    // 개인통관고유부호 (없으면 null 가능)
     @Size(max = 20)
     private String customsCode;
 
-    // 최종 결제 금액 (토스 amount 와 동일해야 함)
     @NotNull
     private Long totalAmount;
 }
