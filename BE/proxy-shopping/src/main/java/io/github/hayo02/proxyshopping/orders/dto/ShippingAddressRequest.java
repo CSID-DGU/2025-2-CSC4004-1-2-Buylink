134a38852e02736a5a42f063d30af03c34aa8167
hayo02 <hayo021111@naver.com>
2025-12-12 00:51:34 +0900

    Refactor: Update ShippingAddressRequest fields and validation

--- BE/proxy-shopping/src/main/java/io/github/hayo02/proxyshopping/orders/dto/ShippingAddressRequest.java
+++ BE/proxy-shopping/src/main/java/io/github/hayo02/proxyshopping/orders/dto/ShippingAddressRequest.java
@@ -14,23 +14,18 @@ public class ShippingAddressRequest {
     @NotBlank
     @Size(max = 50)
     private String receiverName;
-
     @NotBlank
     @Size(max = 20)
     private String phone;
-
     @NotBlank
     @Size(max = 10)
     private String postalCode;
-
     @NotBlank
     @Size(max = 255)
     private String roadAddress;
-
     @NotBlank
     @Size(max = 255)
     private String detailAddress;
-
     @Size(max = 255)
     private String deliveryRequest;
 }
