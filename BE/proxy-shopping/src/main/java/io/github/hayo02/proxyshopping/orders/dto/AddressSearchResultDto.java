6295eaf46b81ed44ef2a35d1d763c10bf001a7b4
hayo02 <hayo021111@naver.com>
2025-12-12 00:40:36 +0900

    Refactor: Update AddressSearchResultDto structure and mapping logic

--- BE/proxy-shopping/src/main/java/io/github/hayo02/proxyshopping/orders/dto/AddressSearchResultDto.java
+++ BE/proxy-shopping/src/main/java/io/github/hayo02/proxyshopping/orders/dto/AddressSearchResultDto.java
@@ -7,12 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public class AddressSearchResultDto {
 
-    // 도로명 주소
     private final String roadAddress;
 
-    // 지번 주소
     private final String jibunAddress;
 
-    // 우편번호
     private final String zipCode;
 }
