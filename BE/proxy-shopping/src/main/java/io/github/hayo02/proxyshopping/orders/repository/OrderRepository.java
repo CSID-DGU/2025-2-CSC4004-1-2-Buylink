b46b64e2d8150005eb3cfa2a6da2512d923abcf0
hayo02 <hayo021111@naver.com>
2025-12-12 01:00:20 +0900

    Refactor: Update OrderRepository interface and query methods

--- BE/proxy-shopping/src/main/java/io/github/hayo02/proxyshopping/orders/repository/OrderRepository.java
+++ BE/proxy-shopping/src/main/java/io/github/hayo02/proxyshopping/orders/repository/OrderRepository.java
@@ -7,9 +7,8 @@ import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    // 기존 메서드 (proxySid 함께 쓰는 용도 – 필요하면 계속 사용)
+
     Optional<Order> findByOrderNumberAndProxySid(String orderNumber, String proxySid);
 
-    // 주문번호만으로 조회 (주문 상세 조회에 사용)
     Optional<Order> findByOrderNumber(String orderNumber);
 }
